2b1caef42685316faf4bdd5b94dd609f4157ba55
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-04-19 18:59:22 +0700

    Set up backend

---
+++ backend/functions/openai_requests.py
@@ -0,0 +1,33 @@
+import openai
+from decouple import config
+
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+from functions.database import get_recent_messages
+
+#Audio to Text
+def convert_audio_to_text(audio_file):
+  try:
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    message_text = transcript["text"]
+    return message_text
+  except Exception as e:
+    return
+  
+#Get response
+def get_chat_response(message_input):
+  messages = get_recent_messages()
+  user_message = {"role": "user", "content": message_input + " Only say two or 3 words in Spanish if speaking in Spanish. The remaining words should be in English"}
+  messages.append(user_message)
+  print(messages)
+
+  try:
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+    message_text = response["choices"][0]["message"]["content"]
+    return message_text
+  except Exception as e:
+    return
